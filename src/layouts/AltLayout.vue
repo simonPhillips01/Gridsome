1d4e4b8b9bd5d3413d7b62648d6f151e7d7cea57
Simon Phillips <simon.phillips01@icloud.com>
2019-11-12 14:19:13 +0000

    Added blog page

--- src/layouts/AltLayout.vue
+++ src/layouts/AltLayout.vue
@@ -7,6 +7,7 @@
       <nav class="nav">
         <g-link exact class="nav__link" to="/">Home</g-link>
         <g-link class="nav__link" to="/about">About</g-link>
+        <g-link class="nav__link" to="/blog">Blog</g-link>
         <g-link class="nav__link" to="/products">Products</g-link>
       </nav>
     </header>
