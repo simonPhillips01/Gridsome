a837dee023d2aa34458221d3ec6f292362064c3f
Simon Phillips <simon.phillips01@icloud.com>
2019-11-14 17:07:42 +0000

    Added comments with Disqus

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -1,6 +1,7 @@
 <template>
     <Layout>
         <div v-html="$page.post.content"/>
+        <vue-disqus shortname="gridsome-3" :identifier="$page.post.id"></vue-disqus>
     </Layout>
 </template>
 
